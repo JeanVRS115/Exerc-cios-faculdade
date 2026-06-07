674261b09c9caff881a8b4f6f9de0989da770d05
Jean Vinício <jeanvinicio.df@gmail.com>
2026-06-07 12:42:27 -0300

    Atividades Willians

---
+++ JVRS-15.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+int main() {
+
+printf("************************************************************\n");
+printf("*JEAN VINICIO RODOVALHO SOARES - 0024931                   *\n");
+printf("Programa JVRS-15 - Quantas caixas cabem dentro do caminhao *\n");
+printf("************************************************************\n");
+
+int quantidade;
+float hcaixa, ccaixa, lcaixa;
+float hcaminhao, ccaminhao, lcaminhao;
+float volumecaixa, volumecaminhao;
+
+printf("!ATENCAO! PARA TODOS OS VALORES NESTE PROGRAMA UTILIZE CENTIMETROS!\n\n");
+
+printf("Digite a altura da caixa: ");
+scanf("%f", &hcaixa);
+printf("Digite o comprimento da caixa: ");
+scanf("%f", &ccaixa);
+printf("Digite a largura da caixa: ");
+scanf("%f", &lcaixa);
+
+printf("Digite a altura do caminhao: ");
+scanf("%f", &hcaminhao);
+printf("Digite o comprimento do caminhao: ");
+scanf("%f", &ccaminhao);
+printf("Digite a largura do caminhao: ");
+scanf("%f", &lcaminhao);
+
+volumecaixa=ccaixa*lcaixa*hcaixa;
+volumecaminhao=ccaminhao*lcaminhao*hcaminhao;
+
+quantidade=volumecaminhao/volumecaixa;
+
+printf("\nCabem %d caixas no caminhao",quantidade);
+
+}
+
