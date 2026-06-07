fc64229ff8cc4433da2e33b40b55659e391a6ec8
Jean Vinício <jeanvinicio.df@gmail.com>
2026-06-07 14:34:48 -0300

    JVRS-17

---
+++ JVRS-17.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+int main() {
+setlocale(LC_ALL,"portuguese");
+
+printf("**********************************************\n");
+printf("*JEAN VINICIO RODOVALHO SOARES - 0024931     *\n");
+printf("Programa JVRS-17 O Sensor do Parque Temático *\n");
+printf("**********************************************\n");
+
+
+int altura;
+
+printf("ATENÇÃO, DIGITE A ALTURA EM CINTÍMETROS\n");
+
+printf("Digite a altura do usuário: ");
+scanf("%d", &altura);
+
+if (altura>140){
+	printf("ACENDA LUZ VERDE");
+
+}
+else {
+	printf("ACENDA LUZ VERMELHA");
+}
+
+return 0;
+}
+
